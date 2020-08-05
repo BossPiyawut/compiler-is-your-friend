be50d40606c798ae24cbb5cd0243cae5ffe967f2
Piyawut Sinchavanwat <piyawut.c@codium.co>
2020-08-05 13:46:31 +0700

    Finish question 2

--- 2.immutability.ts
+++ 2.immutability.ts
@@ -7,11 +7,14 @@ Questions:
 1) Spot and describe the cause of the bug that causes the "divide by zero" to occur.
 2) Now, make a *single* code change on line 14:
 2.1) without changing the number 42;
+- Change let to const
 2.2) such that cause of the bug can be spotted by the compiler at *compile time*.
 3) What did you learn from this exercise?
+- const is Immutable
+- let is Mutable
 */
 function safeDivide(): number {
-    let x: number = 42;
+    const x: number = 42;
 
     if (x == 0) throw new Error("x should not be 0");
 
@@ -19,3 +22,5 @@ function safeDivide(): number {
 
     return 42 / x;
 }
+
+result = safeDivide()
