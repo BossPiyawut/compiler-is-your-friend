1c9d9a142f0043c1e13f5c35943b4637eb340d05
Piyawut Sinchavanwat <piyawut.c@codium.co>
2020-08-05 14:06:41 +0700

    Update question 4

--- 4.composability.ts
+++ 4.composability.ts
@@ -72,4 +72,5 @@ Think. Compare and contrast the:
 Question:
 1) What did you learn from this problem? Why would we want to refactor the code to be
 like the Composable implementation?
+- Make code less redundancy, better to maintain
 */
