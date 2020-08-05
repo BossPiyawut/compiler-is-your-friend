4fc7833e981123d6d1764482d7e702637984aaaa
Piyawut Sinchavanwat <piyawut.c@codium.co>
2020-08-05 13:32:10 +0700

    Finish question 1

--- 1.correctness.ts
+++ 1.correctness.ts
@@ -10,5 +10,19 @@ You can use Google to look up the method calls.
 
 Questions:
 1) How to ensure that the input is a string at compile time?
+scriptAt(str: string)
 2) What did you learn from this exercise?
+- Use the indexOf
+- Write function
+- Assign type to ensure both input type and output type are correct.
+- Check type of variable
 */
+
+function scriptAt(str: string): number{
+    console.log(typeof str)
+    var result = str.indexOf('Script')
+    return result
+}
+
+var result = scriptAt('somethingScriptsomething')
+console.log(result)
